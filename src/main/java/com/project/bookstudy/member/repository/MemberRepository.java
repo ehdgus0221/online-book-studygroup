78393fb22095c9ab2b84b82d027f8e0ad519ad62
DONG <dlaehdgus23@naver.com>
2023-11-02 16:52:05 +0900

    feat : MemberRepository 클래스 작성

---
+++ src/main/java/com/project/bookstudy/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.project.bookstudy.member.repository;
+
+import com.project.bookstudy.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
