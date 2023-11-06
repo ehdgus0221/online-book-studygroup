6e64f413eb100161b86df249d7e34629f0653fa3
DONG <dlaehdgus23@naver.com>
2023-11-06 19:55:16 +0900

    feat : 스터디 그룹 Service 코드 작성

---
+++ src/main/java/com/project/bookstudy/study_group/service/StudyGroupService.java
@@ -0,0 +1,29 @@
+package com.project.bookstudy.study_group.service;
+
+import com.project.bookstudy.member.domain.Member;
+import com.project.bookstudy.member.repository.MemberRepository;
+import com.project.bookstudy.study_group.domain.StudyGroup;
+import com.project.bookstudy.study_group.repository.StudyGroupRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class StudyGroupService {
+
+    private final StudyGroupRepository studyGroupRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public StudyGroupDto createStudyGroup(Long memberId, CreateStudyGroupParam studyGroupParam) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException(ErrorCode.USER_NOT_FOUND.getDescription()));
+
+        StudyGroup savedStudyGroup = studyGroupRepository.save(StudyGroup.from(member, studyGroupParam));
+
+        return StudyGroupDto.fromEntity(savedStudyGroup);
+    }
+}
