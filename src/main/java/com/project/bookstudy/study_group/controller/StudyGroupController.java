d0bcebe3a05083b4b6d5f08dd2a1b63fd977ffdb
DONG <dlaehdgus23@naver.com>
2023-11-07 19:11:47 +0900

    feat : 스터디 그룹 조회 controller 작성

--- src/main/java/com/project/bookstudy/study_group/controller/StudyGroupController.java
+++ src/main/java/com/project/bookstudy/study_group/controller/StudyGroupController.java
@@ -5,13 +5,11 @@ import com.project.bookstudy.study_group.dto.request.CreateStudyGroupRequest;
 import com.project.bookstudy.study_group.dto.response.CreateStudyGroupResponse;
 import com.project.bookstudy.study_group.service.StudyGroupService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -22,6 +20,12 @@ public class StudyGroupController {
 
     private final StudyGroupService studyGroupService;
 
+    /**
+     *
+     * @param authentication
+     * @param request
+     * 스터디 그룹 생성
+     */
     @PostMapping
     public CreateStudyGroupResponse createStudyGroup(Authentication authentication,
                                                      @Valid @RequestBody CreateStudyGroupRequest request) {
@@ -32,4 +36,21 @@ public class StudyGroupController {
                 .leaderId(studyGroupDto.getLeaderId())
                 .build();
     }
+
+    /**
+     *
+     * @param pageable
+     * @param cond
+     * 스터디그룹 전체 조회
+     * StudyGroupSearchCond를 통해 2가지 검색 기능을 지원한다.
+     * 1. StudyGroup을 만든 사람 이름
+     * 2. StudyGroup 제목
+     */
+    @GetMapping
+    public Page<StudyGroupDto> getStudyGroupList (@PageableDefault Pageable pageable,
+                                                  @ModelAttribute StudyGroupSearchCond cond) {
+        return studyGroupService.getStudyGroupList(pageable, cond);
+    }
+
+
 }
