9daa5b701ffeef08ba1216c2fd020e2d606eb48f
DONG <dlaehdgus23@naver.com>
2023-11-14 16:24:05 +0900

    feat : 스터디그룹 신청 controller 작성

---
+++ src/main/java/com/project/bookstudy/enrollment/controller/EnrollmentController.java
@@ -0,0 +1,26 @@
+package com.project.bookstudy.enrollment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/enrollment")
+public class EnrollmentController {
+
+    private final EnrollmentService enrollmentService;
+
+
+    /**
+     *
+     * @param StudyGroupId
+     * @param authentication
+     * 스터디 그룹 신청하기
+     */
+    @PostMapping
+    public ResponseEntity<CreateEnrollmentResponse> createEnrollment(@RequestParam Long StudyGroupId, Authentication authentication) {
+        return ResponseEntity.ok(enrollmentService.enroll(StudyGroupId, authentication));
+    }
+}
