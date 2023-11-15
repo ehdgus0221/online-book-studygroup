246e73ea662eef322f05e049122ce05e880057e5
DONG <dlaehdgus23@naver.com>
2023-11-15 22:22:02 +0900

    test : 스터디 그룹 신청내역 전체조회 성공

--- src/test/java/com/project/bookstudy/enrollment/service/EnrollmentServiceTest.java
+++ src/test/java/com/project/bookstudy/enrollment/service/EnrollmentServiceTest.java
@@ -27,7 +27,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -203,6 +205,58 @@ public class EnrollmentServiceTest {
 
     }
 
+    @Test
+    @DisplayName("스터디 그룹 전체조회 성공")
+    @Transactional
+    void getEnrollmentListSuccess() {
+        //given
+        Member member1 = createMember("member","member@naver.com");
+        memberRepository.save(member1);
+
+        Member leader1 = createMember("leader", "leader@naver.com");
+        memberRepository.save(leader1);
+
+        Authentication authentication1 = createAuthenticationLeader();
+        Authentication authentication2 = createAuthenticationMember();
+
+        Long originMemberPoint = 1000000L;
+        member1.chargePoint(originMemberPoint);
+
+        CreateStudyGroupRequest request1 = createStudyCreateGroupRequest(leader1.getId(),
+                LocalDateTime.of(2023, 12, 1, 0, 0, 0),
+                LocalDateTime.of(2023, 12, 2, 0, 0, 0),
+                LocalDateTime.of(2023, 11, 1, 0, 0, 0),
+                LocalDateTime.of(2023, 11, 30, 0, 0, 0), "subject", "contents");
+        StudyGroupDto response1 = studyGroupService.createStudyGroup(authentication1, request1.toStudyGroupParam());
+
+        CreateStudyGroupRequest request2 = createStudyCreateGroupRequest(leader1.getId(),
+                LocalDateTime.of(2023, 12, 1, 0, 0, 0),
+                LocalDateTime.of(2023, 12, 2, 0, 0, 0),
+                LocalDateTime.of(2023, 11, 1, 0, 0, 0),
+                LocalDateTime.of(2023, 11, 30, 0, 0, 0), "subject", "contents");
+        StudyGroupDto response2 = studyGroupService.createStudyGroup(authentication1, request1.toStudyGroupParam());
+
+        StudyGroup studyGroup1 = studyGroupRepository.findById(response1.getId())
+                .orElseThrow(() -> new IllegalArgumentException("스터디 없음"));
+        StudyGroup studyGroup2 = studyGroupRepository.findById(response2.getId())
+                .orElseThrow(() -> new IllegalArgumentException("스터디 없음"));
+
+
+        Long enrollmentId1 = enrollmentService.enroll(studyGroup1.getId(), authentication2);
+        Long enrollmentId2 = enrollmentService.enroll(studyGroup2.getId(), authentication2);
+
+        Optional<Enrollment> enrollment1 = enrollmentRepository.findById(enrollmentId1);
+        Optional<Enrollment> enrollment2 = enrollmentRepository.findById(enrollmentId2);
+
+        List<Optional<Enrollment>> enrollmentList = new ArrayList<>();
+        enrollmentList.add(enrollment1);
+        enrollmentList.add(enrollment2);
+
+
+        //then
+        assertThat(enrollmentList.size()).isEqualTo(2);
+
+    }
 
 
     /**
