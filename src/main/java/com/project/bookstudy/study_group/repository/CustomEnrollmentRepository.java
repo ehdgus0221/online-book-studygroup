facfe4f1c13ab9338ba4c26ee7b3040507d2785d
DONG <dlaehdgus23@naver.com>
2023-11-15 20:54:55 +0900

    feat : 스터디 그룹 신청내역 전체조회 QueryDSL 작성

--- src/main/java/com/project/bookstudy/study_group/repository/CustomEnrollmentRepository.java
+++ src/main/java/com/project/bookstudy/study_group/repository/CustomEnrollmentRepository.java
@@ -1,9 +1,13 @@
 package com.project.bookstudy.study_group.repository;
 
 import com.project.bookstudy.enrollment.domain.Enrollment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface CustomEnrollmentRepository {
     public Optional<Enrollment> findByIdWithAll(Long id);
+
+    public Page<Enrollment> searchEnrollment(Pageable pageable, Long memberId);
 }
--- src/main/java/com/project/bookstudy/study_group/repository/CustomEnrollmentRepositoryImpl.java
+++ src/main/java/com/project/bookstudy/study_group/repository/CustomEnrollmentRepositoryImpl.java
@@ -4,7 +4,11 @@ import com.project.bookstudy.enrollment.domain.Enrollment;
 import com.project.bookstudy.member.domain.QMember;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.project.bookstudy.enrollment.domain.QEnrollment.enrollment;
@@ -30,5 +34,28 @@ public class CustomEnrollmentRepositoryImpl implements CustomEnrollmentRepositor
                 .fetchOne();
 
         return Optional.ofNullable(result);
+    }
+
+    @Override
+    public Page<Enrollment> searchEnrollment(Pageable pageable, Long memberId) {
+
+        QMember leader = new QMember("m");
+
+        List<Enrollment> enrollmentList = jpaQueryFactory.selectFrom(enrollment)
+                .join(enrollment.studyGroup, studyGroup).fetchJoin()
+                .join(studyGroup.leader, leader).fetchJoin()
+                .join(enrollment.payment, payment).fetchJoin()
+                .join(enrollment.member, member).fetchJoin()
+                .where(member.id.eq(memberId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long total = jpaQueryFactory.select(enrollment.count())
+                .from(enrollment)
+                .where(enrollment.member.id.eq(memberId))
+                .fetchOne();
+
+        return new PageImpl<>(enrollmentList, pageable, total);
     }
 }
