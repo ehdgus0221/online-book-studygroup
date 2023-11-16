0ea8fa0a1f37fa41d68a179d69c4427a60a0bdf8
DONG <dlaehdgus23@naver.com>
2023-11-16 11:02:51 +0900

    feat : 카테고리 생성 controller 작성

---
+++ src/main/java/com/project/bookstudy/category/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.project.bookstudy.category.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/category")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+    @PostMapping
+    public ResponseEntity<CreateCategoryResponse> createCategory(@RequestBody CreateCategoryRequest request) {
+        Long categoryId = categoryService.createCategory(request);
+
+        return ResponseEntity.ok(CreateCategoryResponse);
+    }
+}
