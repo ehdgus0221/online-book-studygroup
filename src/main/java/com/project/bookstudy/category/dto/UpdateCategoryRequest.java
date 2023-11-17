c5a0a8a6cbf92e527e2d651a4befcc32b8d8fb44
DONG <dlaehdgus23@naver.com>
2023-11-17 15:59:57 +0900

    feat : 카테고리 수정 Dto 작성

---
+++ src/main/java/com/project/bookstudy/category/dto/UpdateCategoryRequest.java
@@ -0,0 +1,21 @@
+package com.project.bookstudy.category.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class UpdateCategoryRequest {
+    private Long categoryId;
+    private Long parentCategoryId;
+    private Boolean toRootCategory;
+    private String subject;
+
+    @Builder
+    private UpdateCategoryRequest(Long categoryId, Long parentCategoryId, String subject) {
+        this.categoryId = categoryId;
+        this.parentCategoryId = parentCategoryId;
+        this.subject = subject;
+    }
+}
