355daeda07c605c600f3962d35aeca538036373f
DONG <dlaehdgus23@naver.com>
2023-11-18 14:37:43 +0900

    feat : 게시물, 파일 엔티티 기본 세팅

---
+++ src/main/java/com/project/bookstudy/post/file/domain/File.java
@@ -0,0 +1,45 @@
+package com.project.bookstudy.post.file.domain;
+
+import com.project.bookstudy.common.domain.BaseTimeEntity;
+import com.project.bookstudy.post.domain.Post;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class File extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "file_id")
+    private Long id;
+
+    private String filePath;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private Boolean isDeleted = Boolean.FALSE;
+
+    @Builder
+    private File(String filePath, Post post) {
+        this.filePath = filePath;
+        this.post = post;
+    }
+
+    public static File of(String filePath ,Post post) {
+        File file = File.builder()
+                .filePath(filePath)
+                .post(post)
+                .build();
+
+        post.getFiles().add(file);
+        return file;
+    }
+}
