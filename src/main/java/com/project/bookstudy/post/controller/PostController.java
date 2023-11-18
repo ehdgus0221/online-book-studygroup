137b3d4c8c3686a180481b8241a8dbd619360c23
DONG <dlaehdgus23@naver.com>
2023-11-18 14:49:00 +0900

    feat : 게시물 생성 요청 Dto 작성

--- src/main/java/com/project/bookstudy/post/controller/PostController.java
+++ src/main/java/com/project/bookstudy/post/controller/PostController.java
@@ -1,9 +1,12 @@
 package com.project.bookstudy.post.controller;
 
+import com.project.bookstudy.post.dto.CreatePostRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,7 +17,7 @@ public class PostController {
 
     @PostMapping
     public ResponseEntity<CreatePostResponse> createPost(@RequestPart CreatePostRequest request,
-                                                         @RequestPart(value = "file", required = false) MultipartFile imageFile) {
-        return ResponseEntity.ok(postService.createPost(request,imageFile));
+                                                         @RequestPart(value = "files", required = false) List<MultipartFile> imageFiles) {
+        return ResponseEntity.ok(postService.createPost(request,imageFiles));
     }
 }
---
+++ src/main/java/com/project/bookstudy/post/dto/CreatePostRequest.java
@@ -0,0 +1,17 @@
+package com.project.bookstudy.post.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class CreatePostRequest {
+    private String subject;
+    private String contents;
+    private Long categoryId;
+    private Long studyGroupId;
+
+    private List<CreateFileRequest> files;
+}
