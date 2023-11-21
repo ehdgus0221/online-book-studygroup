bcdeb8955ab463310b949757d5aa60c56a2df0d4
DONG <dlaehdgus23@naver.com>
2023-11-21 19:52:24 +0900

    feat : 게시글 전체조회 controller 작성

--- src/main/java/com/project/bookstudy/post/controller/PostController.java
+++ src/main/java/com/project/bookstudy/post/controller/PostController.java
@@ -5,6 +5,9 @@ import com.project.bookstudy.post.dto.CreatePostResponse;
 import com.project.bookstudy.post.dto.PostDto;
 import com.project.bookstudy.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -43,6 +46,23 @@ public class PostController {
     @GetMapping("/{id}")
     public ResponseEntity<PostDto> getPost(@PathVariable("id") Long postId) {
         return ResponseEntity.ok(postService.getPost(postId));
+    }
+
+    /**
+     *
+     * @param pageable
+     * @param cond
+     * 게시글 전체 조회
+     * cond를 통해 검색 조건 필터 가능
+     * 1. studyGroupId
+     * 2. categoryId;
+     * 3. subject;
+     * 4. contents;
+     */
+    @GetMapping
+    public ResponseEntity<Page<PostDto>> getPosts(@PageableDefault Pageable pageable,
+                                  @ModelAttribute PostSearchCond cond) {
+        return ResponseEntity.ok(postService.getPostList(pageable, cond););
     }
 
 }
