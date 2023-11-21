6b721e1947a2abb177558b1803af79fa583758ff
DONG <dlaehdgus23@naver.com>
2023-11-21 20:06:31 +0900

    feat : 게시글 전체조회 queryDSL 작성

--- src/main/java/com/project/bookstudy/post/repository/CustomPostRepository.java
+++ src/main/java/com/project/bookstudy/post/repository/CustomPostRepository.java
@@ -1,9 +1,13 @@
 package com.project.bookstudy.post.repository;
 
 import com.project.bookstudy.post.domain.Post;
+import com.project.bookstudy.post.dto.PostSearchCond;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface CustomPostRepository {
     Optional<Post> findByIdWithAll(Long id);
+    Page<Post> searchPost(Pageable pageable, PostSearchCond cond);
 }
--- src/main/java/com/project/bookstudy/post/repository/CustomPostRepositoryImpl.java
+++ src/main/java/com/project/bookstudy/post/repository/CustomPostRepositoryImpl.java
@@ -1,9 +1,16 @@
 package com.project.bookstudy.post.repository;
 
 import com.project.bookstudy.post.domain.Post;
+import com.project.bookstudy.post.dto.PostSearchCond;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.util.StringUtils;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.project.bookstudy.category.domain.QCategory.category;
@@ -28,5 +35,42 @@ public class CustomPostRepositoryImpl implements CustomPostRepository{
                 .fetchOne();
 
         return Optional.ofNullable(result);
+    }
+
+    @Override
+    public Page<Post> searchPost(Pageable pageable, PostSearchCond cond) {
+        List<Post> posts = jpaQueryFactory.selectFrom(post)
+                .join(post.studyGroup, studyGroup).fetchJoin()
+                .join(post.category, category).fetchJoin()
+                .join(post.member, member).fetchJoin()
+                .join(post.files, file).fetchJoin()
+                .where(post.studyGroup.id.eq(cond.getStudyGroupId()),   //essential condition
+                        categoryCond(cond.getCategoryId()),
+                        subjectCond(cond.getSubject()),
+                        contentsCond(cond.getContents()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        Long total = jpaQueryFactory.select(post.count())
+                .from(post)
+                .where(post.studyGroup.id.eq(cond.getStudyGroupId()),
+                        categoryCond(cond.getCategoryId()),
+                        subjectCond(cond.getSubject()),
+                        contentsCond(cond.getContents()))
+                .fetchOne();
+
+        return new PageImpl(posts, pageable, total);
+    }
+
+    private BooleanExpression categoryCond(Long categoryId) {
+        return categoryId != null ? post.category.id.eq(categoryId) : null;
+    }
+    private BooleanExpression subjectCond(String subject) {
+        return StringUtils.hasText(subject) ? post.subject.contains(subject) : null;
+    }
+
+    private BooleanExpression contentsCond(String contents) {
+        return StringUtils.hasText(contents) ? post.subject.contains(contents) : null;
     }
 }
