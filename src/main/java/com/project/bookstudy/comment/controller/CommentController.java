081d0b023e718ea0c82fd20b66fcf1594969c6d3
DONG <dlaehdgus23@naver.com>
2023-11-24 12:16:20 +0900

    feat : 댓글 조회 controller 작성

--- src/main/java/com/project/bookstudy/comment/controller/CommentController.java
+++ src/main/java/com/project/bookstudy/comment/controller/CommentController.java
@@ -6,10 +6,7 @@ import com.project.bookstudy.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +15,21 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    /**
+     *
+     * @param request
+     * @param authentication
+     * 댓글 작성
+     */
     @PostMapping
     public ResponseEntity<CreateCommentResponse> createComment(@RequestBody CreateCommentRequest request
     , Authentication authentication) {
         return ResponseEntity.ok(commentService.createComment(request, authentication));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<CommentResponse> getRootOrChildComment(@RequestParam(required = false) Long parentId) {
+        return ResponseEntity.ok(commentService.getRootOrChildCommentList(parentId));
     }
 }
