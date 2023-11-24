3b275ba31252f75a9f8c9be510bb7e0a75eb4099
DONG <dlaehdgus23@naver.com>
2023-11-24 10:05:50 +0900

    feat : 댓글 작성 controller 작성

---
+++ src/main/java/com/project/bookstudy/comment/controller/CommentController.java
@@ -0,0 +1,23 @@
+package com.project.bookstudy.comment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comment")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CreateCommentResponse> createComment(@RequestBody CreateCommentRequest request
+    , Authentication authentication) {
+        return ResponseEntity.ok(commentService.createComment(request));
+    }
+}
