e4ee12899dd352217b8ad06e52407e550dba6de1
DONG <dlaehdgus23@naver.com>
2023-11-26 21:04:14 +0900

    feat : 스케쥴러를 통한 스터디그룹 상태 업데이트

---
+++ src/main/java/com/project/bookstudy/enrollment/service/StudyGroupQuartzScheduler.java
@@ -0,0 +1,35 @@
+package com.project.bookstudy.enrollment.service;
+
+import com.project.bookstudy.study_group.domain.StudyGroup;
+import com.project.bookstudy.study_group.repository.StudyGroupRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@EnableScheduling
+@RequiredArgsConstructor
+public class StudyGroupQuartzScheduler {
+    /**
+     * 스케쥴러 Cron
+     * 스터디 모집 기간, 스터디 진행 기간에 따른 스터디그룹 상태 업데이트
+     * 매일 자정 (0시)에 진행
+     */
+    private final StudyGroupRepository studyGroupRepository;
+
+    @Scheduled(cron = "* * 0 * * *")
+    @Transactional
+    public void checkStudyGroupStatus() {
+        List<StudyGroup> studyGroupList = studyGroupRepository.findAll();
+
+        for (StudyGroup studygroup : studyGroupList) {
+            studygroup.updateStatus();
+        }
+    }
+}
