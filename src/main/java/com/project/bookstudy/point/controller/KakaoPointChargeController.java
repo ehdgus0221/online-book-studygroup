16824739cc2451b7b5a5acb2e9fcc511e66723d7
DONG <dlaehdgus23@naver.com>
2023-11-26 19:36:43 +0900

    feat : 포인트 충전 취소 controller 작성

--- src/main/java/com/project/bookstudy/point/controller/KakaoPointChargeController.java
+++ src/main/java/com/project/bookstudy/point/controller/KakaoPointChargeController.java
@@ -3,20 +3,21 @@ package com.project.bookstudy.point.controller;
 import com.project.bookstudy.point.api.ApiResponse;
 import com.project.bookstudy.point.controller.dto.KakaoPointChargePrepareResponse;
 import com.project.bookstudy.point.controller.dto.PointChargePrepareRequest;
+import com.project.bookstudy.point.domain.PointChargeStatus;
 import com.project.bookstudy.point.service.PointChargeService;
 import com.project.bookstudy.point.service.dto.PointChargePrepareServiceResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/kakao/point")
 public class KakaoPointChargeController {
 
     private final PointChargeService pointChargeService;
 
-    @PostMapping("/api/v1/kakao/point/ready")
+    @PostMapping("/ready")
     public ApiResponse<KakaoPointChargePrepareResponse> getPointChargeUrl(@RequestBody PointChargePrepareRequest request) {
 
         PointChargePrepareServiceResponse response = pointChargeService.prepare(request.getMemberId(), request.getPointAmount());
@@ -24,4 +25,11 @@ public class KakaoPointChargeController {
 
         return ApiResponse.ok(kakaoPointChargePrepareResponse);
     }
+
+    @RequestMapping("/cancel")
+    public ApiResponse<Void> cancelPointCharge(@RequestParam("temp_key") String tempKey) {
+        pointChargeService.terminate(tempKey, PointChargeStatus.CANCEL);
+        return ApiResponse.of(HttpStatus.ACCEPTED, "결제가 취소되었습니다.", null);
+    }
 }
+
