f42fc7290a8684c833de06a41b0eaf88ae9ca790
DONG <dlaehdgus23@naver.com>
2023-11-26 20:14:02 +0900

    feat : 포인트 충전 승인 controller 작성

--- src/main/java/com/project/bookstudy/point/controller/KakaoPointChargeController.java
+++ src/main/java/com/project/bookstudy/point/controller/KakaoPointChargeController.java
@@ -4,11 +4,15 @@ import com.project.bookstudy.point.api.ApiResponse;
 import com.project.bookstudy.point.controller.dto.KakaoPointChargePrepareResponse;
 import com.project.bookstudy.point.controller.dto.PointChargePrepareRequest;
 import com.project.bookstudy.point.domain.PointChargeStatus;
+import com.project.bookstudy.point.infra.KakaoPointClient;
 import com.project.bookstudy.point.service.PointChargeService;
+import com.project.bookstudy.point.service.dto.PointChargeDto;
 import com.project.bookstudy.point.service.dto.PointChargePrepareServiceResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,6 +40,14 @@ public class KakaoPointChargeController {
     public ApiResponse<Void> failPointCharge(@RequestParam("temp_key") String tempKey) {
         pointChargeService.terminate(tempKey, PointChargeStatus.FAIL);
         return ApiResponse.of(HttpStatus.SERVICE_UNAVAILABLE, "결제가 실패했습니다.", null);
+    }
+
+    @GetMapping("/approval/success")
+    public ApiResponse<PointChargeDto> approvePointCharge(@RequestParam("temp_key") String tempKey, @RequestParam("pg_token") String token) {
+        Map<String, String> extraRequestData = Map.of(KakaoPointClient.PG_TOKEN_KEY, token);
+        PointChargeDto pointChargeDto = pointChargeService.approve(tempKey, extraRequestData);
+
+        return ApiResponse.ok(pointChargeDto);
     }
 }
 
