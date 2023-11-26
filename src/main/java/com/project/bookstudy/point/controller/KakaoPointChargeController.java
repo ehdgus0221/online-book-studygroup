5eeaf6749db3c350bed7f7e645a50cdcd45a254b
DONG <dlaehdgus23@naver.com>
2023-11-26 16:35:25 +0900

    feat : 포인트 결제 준비 API controller 관련 클래스 import

--- src/main/java/com/project/bookstudy/point/controller/KakaoPointChargeController.java
+++ src/main/java/com/project/bookstudy/point/controller/KakaoPointChargeController.java
@@ -1,6 +1,8 @@
 package com.project.bookstudy.point.controller;
 
 import com.project.bookstudy.point.api.ApiResponse;
+import com.project.bookstudy.point.controller.dto.KakaoPointChargePrepareResponse;
+import com.project.bookstudy.point.service.dto.PointChargePrepareServiceResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +15,7 @@ public class KakaoPointChargeController {
     private final PointChargeService pointChargeService;
 
     @PostMapping("/api/v1/kakao/point/ready")
-    public ApiResonse<KakaoPointChargePrepareResponse> getPointChargeUrl(@RequestBody PointChargePrepareRequest request) {
+    public ApiResponse<KakaoPointChargePrepareResponse> getPointChargeUrl(@RequestBody PointChargePrepareRequest request) {
 
         PointChargePrepareServiceResponse response = pointChargeService.prepare(request.getMemberId(), request.getPointAmount());
         KakaoPointChargePrepareResponse kakaoPointChargePrepareResponse = KakaoPointChargePrepareResponse.fromServiceResponse(response);
